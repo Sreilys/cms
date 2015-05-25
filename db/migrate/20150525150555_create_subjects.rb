4bb2d58db04c26e2e724b4ad2bd7cb50630c38a8
Sabrina Bosco <sabrina@wonderweblabs.com>
2015-05-25 17:22:08 +0200

    create parameters for db tables and migrations

--- db/migrate/20150525150555_create_subjects.rb
+++ db/migrate/20150525150555_create_subjects.rb
@@ -1,6 +1,9 @@
-class CreateSubjects < ActiveRecord::Migration
+ class CreateSubjects < ActiveRecord::Migration
   def change
     create_table :subjects do |t|
+      t.string "name"
+      t.string "position"
+      t.boolean "visible", default: false
 
       t.timestamps null: false
     end
--- db/migrate/20150525150603_create_pages.rb
+++ db/migrate/20150525150603_create_pages.rb
@@ -1,8 +1,16 @@
 class CreatePages < ActiveRecord::Migration
   def change
     create_table :pages do |t|
+      # add id for let the table know what the corespondent subject is - t.references :subject
+      t.integer "subject_id"
+      t.string "name"
+      t.string "permalink"
+      t.string "position"
+      t.boolean "visible", default: false
 
       t.timestamps null: false
     end
+    add_index("pages", "subject_id")
+    add_index("pages", "permalink")
   end
 end
