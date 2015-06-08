4ee67bc74451dacfa6240b26125cc7ea0e6e60fe
Sabrina Bosco <sabrina@wonderweblabs.com>
2015-06-08 17:04:33 +0200

    deleted routes from the generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'subjects/index'
-
-  get 'subjects/show'
-
-  get 'subjects/new'
-
-  get 'subjects/edit'
-
-  get 'subjects/delete'
-
   root "pages#index"
 
   resources :pages
